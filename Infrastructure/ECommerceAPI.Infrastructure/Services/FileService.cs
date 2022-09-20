f0ea786cf10f6229f17c1f0ac816124267228a25
Batu YURT <asdasd018@hotmail.com>
2022-09-20 21:38:06 +0300

    File registration, SEO optimization basics

---
+++ Infrastructure/ECommerceAPI.Infrastructure/Services/FileService.cs
@@ -0,0 +1,80 @@
+﻿using ECommerceAPI.Application.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Infrastructure.Services
+{
+    public class FileService : IFileService
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public FileService(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public async Task<bool> CopyFileAsync(string path, IFormFile file)
+        {
+            try
+            {
+                await using FileStream fileStream = new(path, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
+
+                await file.CopyToAsync(fileStream);
+
+                await fileStream.FlushAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        async Task<string> FileRenameAsync(string fileName)
+        {
+            string extension = Path.GetExtension(fileName);
+            string oldName = Path.GetFileNameWithoutExtension(fileName);
+            Regex regex = new Regex("[*'\",+-._&#^@|/<>~]");
+            string newFileName = regex.Replace(oldName, string.Empty);
+            DateTime datetimenow = DateTime.UtcNow;
+            string datetimeutcnow = datetimenow.ToString("yyyyMMddHHmmss");
+            string fullName = $"{datetimeutcnow}-{newFileName}{extension}";
+
+            return fullName;
+        }
+
+        public async Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files)
+        {
+
+            string uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, path);
+            if (!Directory.Exists(uploadPath))
+                Directory.CreateDirectory(uploadPath);
+
+            List<(string fileName, string path)> datas = new();
+
+            List<bool> results = new();
+            foreach (IFormFile file in files)
+            {
+                string fileNewName = await FileRenameAsync(file.FileName);
+
+                bool result = await CopyFileAsync($"{uploadPath}\\{fileNewName}", file);
+                datas.Add((fileNewName, $"{uploadPath}\\{fileNewName}"));
+                results.Add(result);
+            }
+
+            if (results.TrueForAll(r => r.Equals(true)))
+                return datas;
+
+            return null;
+
+        }
+    }
+}
