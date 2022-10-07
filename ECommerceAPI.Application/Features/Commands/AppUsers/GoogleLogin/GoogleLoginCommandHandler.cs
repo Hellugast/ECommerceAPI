0b303982e93e576f9faab50fe03f83888df9191a
Batu YURT <asdasd018@hotmail.com>
2022-10-07 20:28:47 +0300

    Google login

---
+++ ECommerceAPI.Application/Features/Commands/AppUsers/GoogleLogin/GoogleLoginCommandHandler.cs
@@ -0,0 +1,72 @@
+﻿using ECommerceAPI.Application.Abstractions.Tokens;
+using ECommerceAPI.Application.DTOs;
+using ECommerceAPI.Domain.Entities.Identity;
+using Google.Apis.Auth;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Application.Features.Commands.AppUsers.GoogleLogin
+{
+    public class GoogleLoginCommandHandler : IRequestHandler<GoogleLoginCommandRequest, GoogleLoginCommandResponse>
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ITokenHandler _tokenHandler;
+
+        public GoogleLoginCommandHandler(UserManager<AppUser> userManager, ITokenHandler tokenHandler)
+        {
+            _userManager = userManager;
+            _tokenHandler = tokenHandler;
+        }
+
+        public async Task<GoogleLoginCommandResponse> Handle(GoogleLoginCommandRequest request, CancellationToken cancellationToken)
+        {
+            var settings = new GoogleJsonWebSignature.ValidationSettings()
+            {
+                Audience = new List<string> { "-" }
+            };
+
+            var payload = await GoogleJsonWebSignature.ValidateAsync(request.IdToken, settings);
+
+            var info = new UserLoginInfo(request.Provider, payload.Subject, request.Provider);
+
+            AppUser user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
+
+            bool result = user != null;
+            if (user == null)
+            {
+                user = await _userManager.FindByEmailAsync(payload.Email);
+                if (user == null)
+                {
+                    user = new AppUser()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Email = payload.Email,
+                        UserName = payload.Email,
+
+
+                    };
+                    var identityResult = await _userManager.CreateAsync(user);
+                    result = identityResult.Succeeded;
+                }
+            }
+
+            if (result)
+                await _userManager.AddLoginAsync(user, info);
+            else
+                throw new Exception("Invalid external authentication");
+
+            Token token = _tokenHandler.CreateAccessToken(5);
+
+            return new()
+            {
+                Token = token
+            };
+
+        }
+    }
+}
--- Presentation/ECommerceAPI.API/Controllers/UsersController.cs
+++ Presentation/ECommerceAPI.API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerceAPI.Application.Features.Commands.AppUsers.CreateUser;
+using ECommerceAPI.Application.Features.Commands.AppUsers.GoogleLogin;
 using ECommerceAPI.Application.Features.Commands.AppUsers.LoginUser;
 using MediatR;
 using Microsoft.AspNetCore.Http;
@@ -31,6 +32,13 @@ namespace ECommerceAPI.API.Controllers
         public async Task<IActionResult> Login(LoginUserCommandRequest loginUserCommandRequest)
         {
             LoginUserCommandResponse response = await _mediator.Send(loginUserCommandRequest);
+            return Ok(response);
+        }
+
+        [HttpPost("google-login")]
+        public async Task<IActionResult> GoogleLogin(GoogleLoginCommandRequest googleLoginCommandRequest)
+        {
+            GoogleLoginCommandResponse response = await _mediator.Send(googleLoginCommandRequest);
             return Ok(response);
         }
     }
