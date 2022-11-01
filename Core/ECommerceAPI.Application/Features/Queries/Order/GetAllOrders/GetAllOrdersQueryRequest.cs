979df1831d99fbdecb91cbc064c115bd50b8e4f3
Batu YURT <asdasd018@hotmail.com>
2022-11-01 19:48:47 +0300

    Getorderbyid service

--- Core/ECommerceAPI.Application/Features/Queries/Order/GetAllOrders/GetAllOrdersQueryRequest.cs
+++ Core/ECommerceAPI.Application/Features/Queries/Order/GetAllOrders/GetAllOrdersQueryRequest.cs
@@ -1,7 +1,7 @@
 ﻿using ECommerceAPI.Application.RequestParameters;
 using MediatR;
 
-namespace ECommerceAPI.Application.Features.Queries.Order
+namespace ECommerceAPI.Application.Features.Queries.Order.GetAllOrders
 {
     public class GetAllOrdersQueryRequest : Pagination, IRequest<GetAllOrdersQueryResponse>
     {
---
+++ Core/ECommerceAPI.Application/Features/Queries/Order/GetOrderById/GetOrderByIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using ECommerceAPI.Application.Abstractions.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Application.Features.Queries.Order.GetOrderById
+{
+    public class GetOrderByIdQueryHandler : IRequestHandler<GetOrderByIdQueryRequest, GetOrderByIdQueryResponse>
+    {
+
+        private readonly IOrderService _orderService;
+
+        public GetOrderByIdQueryHandler(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public async Task<GetOrderByIdQueryResponse> Handle(GetOrderByIdQueryRequest request, CancellationToken cancellationToken)
+        {
+            var data = await _orderService.GetOrderById(request.Id);
+            return new()
+            {
+                Id = data.Id,
+                OrderCode = data.OrderCode,
+                Description = data.Description,
+                CreatedDate = data.CreatedDate,
+                Address = data.Address,
+                BasketItems = data.BasketItems
+            };
+        }
+    }
+}
